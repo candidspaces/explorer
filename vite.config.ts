e364e76b3a9ea73daee9f331e0f4d3c940002f98
verbospatial <just.paths.only@gmail.com>
2026-05-07 10:25:42 +0000

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/timeline/',
+  base: '/explorer/',
   plugins: [react()],
   test: {
     globals: true,
